88da3b1af249a9bb10b7906b1a9c19ca3c9c5d4f
NathanNeelis <nathan.neelis@hva.nl>
2021-03-16 21:18:04 +0100

    update gps pointer

--- script.js
+++ script.js
@@ -11,17 +11,59 @@ const kaart = new ol.Map({
     })
 });
 
-
-const laag = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        features: [
-            new ol.Feature({
-                geometry: new ol.geom.Point(
-                    ol.proj.fromLonLat([4.35247, 50.84673])
-                )
-            })
-        ]
+// styling
+let pointStyling = new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 8,
+        fill: new ol.style.Fill({
+            color: "red"
+        }),
+        stroke: new ol.style.Stroke({
+            color: 'black',
+            width: 2,
+        })
     })
-});
+})
 
-kaart.addLayer(laag)
+
+// punt Nathan thuis
+// const nathanHome = new ol.layer.Vector({
+//     source: new ol.source.Vector({
+//         features: [
+//             new ol.Feature({
+//                 geometry: new ol.geom.Point(
+//                     ol.proj.fromLonLat([4.7705690999999995, 52.673312499999994])
+//                 )
+//             })
+//         ]
+//     })
+// });
+
+
+
+// mijn locatie
+navigator.geolocation.getCurrentPosition(getMyLocation);
+
+function getMyLocation(position) {
+    let long = position.coords.longitude;
+    let lat = position.coords.latitude;
+
+    console.log('long ', long, 'lat ', lat)
+
+    let myPosition = new ol.layer.Vector({
+        style: pointStyling,
+        source: new ol.source.Vector({
+            features: [
+                new ol.Feature({
+                    geometry: new ol.geom.Point(
+                        ol.proj.fromLonLat([long, lat])
+                    )
+                })
+            ]
+        })
+    });
+
+    kaart.addLayer(myPosition)
+
+
+}
